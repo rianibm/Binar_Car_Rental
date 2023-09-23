ecd3f34530caea627a9701f2768c59df536fa70c
Riani BM <rianimatialu@gmail.com>
2023-09-24 01:11:47 +0700

    update from codepen

---
+++ script.js
@@ -0,0 +1,24 @@
+// script.js
+//testimonial belum
+
+//faq
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
