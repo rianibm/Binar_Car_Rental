07a09130be4c36035956e6bcf62ecf11d2b30dd2
Riani BM <rianimatialu@gmail.com>
2023-11-10 18:42:36 +0700

    update port

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,6 @@ app.use(express.json());
 
 app.use("/", carRoute); // Mount the carRoute under '/api'
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
 });
