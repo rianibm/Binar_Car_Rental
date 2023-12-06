bc5097f63934b4fffa295f91994215e6cbdbf536
Riani BM <rianimatialu@gmail.com>
2023-12-06 22:01:48 +0700

    Remove all files

--- generateSecretKey.mjs
+++
@@ -1,8 +0,0 @@
-// generateSecretKey.js
-import { randomBytes } from "crypto";
-
-const generateSecretKey = () => {
-  return randomBytes(32).toString("hex");
-};
-
-console.log(generateSecretKey());
