8af96b6d5606b20d369be479453b4447cb5f0e12
Riani BM <rianimatialu@gmail.com>
2024-02-22 00:42:50 +0700

    static login done

--- src/pages/AdminLogin.tsx
+++ src/pages/AdminLogin.tsx
@@ -1,13 +1,83 @@
 // src/pages/AdminLogin.tsx
 import React from "react";
+import { Form, Input, Button } from "antd/lib";
+import { useForm } from "antd/lib/form/Form";
 
 const AdminLogin: React.FC = () => {
-  // Implementasi halaman login admin
+  const [form] = useForm();
+  const handleLogin = (values: any) => {
+    console.log("Received values:", values);
+  };
+  const onValuesChange = () => {
+    // Re-validate the form when values change
+    form.validateFields();
+  };
+
   return (
-    <>
-      <h1>Admin Login</h1>
-      {/* Form login */}
-    </>
+    <div className="flex gap-2 ">
+      <img
+        src="/public/image/login-img.svg"
+        alt=""
+        className="hidden md:block w-[67.1%] h-screen"
+      />
+      <div className="flex flex-col gap-2 justify-center mx-auto mt-[120px]">
+        <div className="bg-primary-background w-[100px] h-[34px]" />
+        <h1 className="text-xl text-black font-helvetica text-bold">
+          Welcome, Admin BCR
+        </h1>
+        <Form
+          form={form}
+          name="login-form"
+          layout="vertical"
+          size="large"
+          initialValues={{ remember: true }}
+          onFinish={handleLogin}
+          onValuesChange={onValuesChange}
+          className="font-helvetica"
+          style={{ maxWidth: "300px", margin: "auto" }}
+        >
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              { required: true, message: "Masukkan email terlebih dahulu" },
+              {
+                type: "email",
+                message: "Email tidak valid",
+              },
+            ]}
+            hasFeedback
+          >
+            <Input placeholder="Contoh: johndee@gmail.com" />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              { required: true, message: "Masukkan password terlebih dahulu" },
+              { min: 6, message: "Password harus minimal 6 digit" },
+            ]}
+            hasFeedback
+          >
+            <Input.Password placeholder="6+ karakter" />
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              htmlType="submit"
+              style={{ width: "100%" }}
+              className="bg-primary text-white cursor-pointer"
+              disabled={
+                !form.getFieldValue("email") || !form.getFieldValue("password")
+              }
+            >
+              Sign In
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,68 @@
-// tailwind.config.js
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          background: "#CFD4ED",
+          DEFAULT: "#0D28A6",
+          dark: "#091B6F",
+        },
+        alert: {
+          danger: "#FA2C5A",
+          warning: "#F9CC00",
+          success: "#73CA5C",
+        },
+        info: {
+          light: "#6de1f0",
+          DEFAULT: "#299FD1",
+        },
+        neutral: {
+          link: "#485466",
+          background: "#F2F4F7",
+          light: "#d0d5dd",
+          gray: "#808991",
+          DEFAULT: "#677084",
+        },
+        disabled: {
+          DEFAULT: "#CFD4ED",
+        },
+        base: {
+          white: "#fff",
+          black: "#151515",
+        },
+      },
+      fontFamily: {
+        helvetica: ["Helvetica", "sans-serif"],
+      },
+      fontSize: {
+        xs: "12px",
+        sm: "14px",
+        base: "16px",
+        lg: "18px",
+        xl: "20px",
+        "2xl": "24px",
+        "3xl": "30px",
+        "4xl": "36px",
+        "5xl": "48px",
+        "6xl": "64px",
+      },
+      heading: {
+        1: "40px",
+        2: "32px",
+        3: "30px",
+        4: "24px",
+        5: "20px",
+        6: "16px",
+      },
+      body: {
+        small: "14px",
+        regular: "16px",
+        large: "18px",
+      },
+    },
   },
+  variants: {},
   plugins: [],
 };
