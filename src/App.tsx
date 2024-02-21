56d2c0e875e7f81a249fb17ecaabd2b9f4a67dd6
Riani BM <rianimatialu@gmail.com>
2024-02-22 00:48:44 +0700

    routes done

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,41 @@
 // src/App.tsx
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Navbar from "./components/Navbar";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
 import CarManagement from "./pages/CarManagement";
+
+const isAuthenticated = () => {
+  // Implement logic to check if the user is authenticated
+  // dummy check here
+  const userToken = localStorage.getItem("userToken");
+  return Boolean(userToken);
+};
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
+        {/* Redirect to login if not authenticated */}
+        <Route
+          path="/"
+          element={
+            isAuthenticated() ? (
+              <Navigate to="/admin-dashboard" />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        <Route path="/CarManagement" element={<CarManagement />} />
+        <Route path="/car-management" element={<CarManagement />} />
         <Route path="/login" element={<AdminLogin />} />
-        {/* <Route path="/" element={<UserLandingPage />} /> */}
       </Routes>
     </Router>
   );
